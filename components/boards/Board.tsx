d55cacf9bcb48b4e1ce6f2bc920d2f41b601971b
CitrusSoda <dndhakus1004@naver.com>
2024-06-08 04:18:40 +0900

    ♻️ refactor: dropdown을 compound패턴으로 바꾸었습니다

--- components/boards/Board.tsx
+++ components/boards/Board.tsx
@@ -1,23 +1,23 @@
 import axios from '@/lib/axios';
-import arrowDownIcon from '@/public/ic_arrow_down.png';
 import heartIcon from '@/public/ic_heart.png';
 import profileIcon from '@/public/ic_profile.png';
 import searchIcon from '@/public/ic_search.png';
-import sortIcon from '@/public/ic_sort.png';
 import { BoardList } from '@/types/board';
 import { formatDate } from '@/utils/formatDate';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect, useRef, useState } from 'react';
 
-export default function Board() {
-  // 화면 클릭 시 드롭다운 닫기를 위한 ref
-  const dropDownRef = useRef<HTMLDivElement | null>(null);
+import {
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+} from './Dropdown';
 
+export default function Board() {
   // 게시글 저장을 위한 state
   const [board, setBoard] = useState<BoardList[] | null>(null);
-  // 인풋 옆 dropdown을 위한 state
-  const [isOpen, setIsOpen] = useState<boolean>(false);
   // dropdown을 다루기 위한 state
   const [boardOrder, setBoardOrder] = useState<string>('recent');
   // 검색을 위한 state
@@ -35,30 +35,6 @@ export default function Board() {
     };
     loadBoard();
   }, [boardOrder]);
-
-  // dropdown 밖 영역 클릭 시 닫기
-  useEffect(() => {
-    const clickOutside = (e: MouseEvent) => {
-      if (
-        dropDownRef.current !== null &&
-        !dropDownRef.current.contains(e.target as Node)
-      ) {
-        setIsOpen(false);
-      }
-    };
-
-    if (isOpen) {
-      window.addEventListener('mousedown', clickOutside);
-    }
-    return () => {
-      window.removeEventListener('mousedown', clickOutside);
-    };
-  }, [isOpen]);
-
-  // dropdown 토글
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
 
   // 검색
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -97,37 +73,19 @@ export default function Board() {
               className="absolute left-4 top-1/2 -translate-y-1/2 transform"
             />
           </div>
-          <div
-            className="relative flex cursor-pointer items-center justify-center rounded-xl border"
-            onClick={toggleDropdown}
-          >
-            <Image src={sortIcon} alt="sort icon" className="sm:hidden" />
-            <div className="hidden w-40 items-center justify-between px-5 sm:flex">
+          <Dropdown>
+            <DropdownToggle>
               <p>{boardOrder === 'recent' ? '최신순' : '좋아요순'}</p>
-              <Image src={arrowDownIcon} alt="down arrow icon" />
-            </div>
-            {/* 드롭다운 */}
-            {isOpen && (
-              <div
-                ref={dropDownRef}
-                className="absolute right-0 top-12 z-50 flex w-32 flex-col rounded-xl border bg-white sm:w-full"
-              >
-                <button
-                  onClick={() => setBoardOrder('recent')}
-                  className="h-[42px]"
-                >
-                  최신순
-                </button>
-                <hr />
-                <button
-                  onClick={() => setBoardOrder('like')}
-                  className="h-[42px]"
-                >
-                  좋아요순
-                </button>
-              </div>
-            )}
-          </div>
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem onClick={() => setBoardOrder('recent')}>
+                최신순
+              </DropdownItem>
+              <DropdownItem onClick={() => setBoardOrder('like')}>
+                좋아요순
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
         </div>
       </div>
       <div className="mt-6">
---
+++ components/boards/Dropdown.tsx
@@ -0,0 +1,126 @@
+import Image from 'next/image';
+import { createContext, useContext, useEffect, useRef, useState } from 'react';
+
+interface DropdownContextProps {
+  isOpen: boolean;
+  closeDropdown: () => void;
+  toggleDropdown: () => void;
+}
+
+interface DropdownProps {
+  children: React.ReactNode;
+}
+
+interface DropdownItemProps {
+  children: React.ReactNode;
+  onClick: () => void;
+}
+
+const DropdownContext = createContext<DropdownContextProps>({
+  isOpen: false,
+  closeDropdown: () => {},
+  toggleDropdown: () => {},
+});
+
+export const Dropdown: React.FC<DropdownProps> = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeDropdown = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <DropdownContext.Provider value={{ isOpen, closeDropdown, toggleDropdown }}>
+      <div className="relative cursor-pointer items-center justify-center rounded-xl border">
+        {children}
+      </div>
+    </DropdownContext.Provider>
+  );
+};
+
+export const DropdownToggle: React.FC<DropdownProps> = ({ children }) => {
+  const { toggleDropdown } = useContext(DropdownContext);
+
+  return (
+    <div onClick={() => toggleDropdown()} className="flex h-full">
+      <Image
+        src="/ic_sort.png"
+        alt="sort icon"
+        className="sm:hidden"
+        width={42}
+        height={42}
+      />
+      <div className="hidden w-32 items-center justify-between px-5 sm:flex">
+        {children}
+        <Image
+          src="/ic_arrow_down.png"
+          alt="down arrow icon"
+          width={24}
+          height={24}
+        />
+      </div>
+    </div>
+  );
+};
+
+export const DropdownMenu: React.FC<DropdownProps> = ({ children }) => {
+  const { isOpen, closeDropdown } = useContext(DropdownContext);
+
+  // 화면 클릭 시 드롭다운 닫기를 위한 ref
+  const dropDownRef = useRef<HTMLDivElement>(null);
+
+  // dropdown 밖 영역 클릭 시 닫기
+  useEffect(() => {
+    // ! e의 타입을 MouseEvent, React.MouseEvent 등 다양하게 줘보았으나 해결하지 못하여
+    // ! unknwon으로 설정하고 instanceof로 MouseEvent일 때 접근가능하도록 하였습니다.
+    const clickOutside = (e: unknown) => {
+      if (
+        dropDownRef.current !== null &&
+        e instanceof MouseEvent &&
+        !dropDownRef.current.contains(e.target as Node)
+      ) {
+        closeDropdown();
+      }
+    };
+
+    if (isOpen) {
+      window.addEventListener('mousedown', clickOutside);
+    }
+    return () => {
+      window.removeEventListener('mousedown', clickOutside);
+    };
+  }, [isOpen, closeDropdown]);
+
+  return isOpen ? (
+    <div
+      className="absolute right-0 top-12 rounded-xl border bg-white shadow-lg"
+      ref={dropDownRef}
+    >
+      {children}
+    </div>
+  ) : null;
+};
+
+export const DropdownItem: React.FC<DropdownItemProps> = ({
+  children,
+  onClick,
+}) => {
+  const { toggleDropdown } = useContext(DropdownContext);
+
+  return (
+    <button
+      className="flex h-[42px] w-32 items-center justify-center"
+      onClick={() => {
+        onClick();
+        toggleDropdown();
+      }}
+    >
+      <hr />
+      {children}
+    </button>
+  );
+};
